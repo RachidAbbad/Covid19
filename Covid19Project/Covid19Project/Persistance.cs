e01e892679eb9b787460ee4d2fca93b59ad5aef4
Rachid-Abbad <75077897+Rachid-Abbad@users.noreply.github.com>
2021-01-11 21:56:10 +0100

    Update Persistance.cs
    Co-Authored-By: AdilHachmoud <59420916+AdilHachmoud@users.noreply.github.com>

--- Covid19Project/Covid19Project/Persistance.cs
+++ Covid19Project/Covid19Project/Persistance.cs
@@ -1,6 +1,8 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,22 +16,26 @@ namespace Covid19Project
         private string user = "root";
         private string pass = "";
         private string db = "covid19";
-        private MySqlConnection cnn;
+        private MySqlConnection cnx;
         private MySqlCommand cmd;
+        private MySqlDataAdapter sda;
         //Constructeur
-        public Persistance(MySqlCommand _cmd)
+        public Persistance()
         {
             initConnection();
+            cmd = new MySqlCommand();
+            sda = new MySqlDataAdapter();
+            cmd.Connection = cnx;
         }
         //Connection au serveur
         public void initConnection()
         {
             string StringConn = "server=" + serveur + ";database=" + db + ";uid=" + user + ";pwd=" + pass + ";";
-            cnn = new MySqlConnection(StringConn);
+            cnx = new MySqlConnection(StringConn);
             try
             {
-                cnn.Open();
-                cnn.Close();
+                cnx.Open();
+                cnx.Close();
             }
             catch (Exception ex)
             {
@@ -41,12 +47,13 @@ namespace Covid19Project
         {
             try
             {
+                cnx.Open();
                 cmd.CommandText = "INSERT INTO suspect(cinsuspect) VALUES ('" + cin + "');";
                 cmd.ExecuteNonQuery();
-                changeGravite(cin, "Moyen");
                 MessageBox.Show("Le citoyen a été ajouté à la liste des suspects");
+                cnx.Close();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
@@ -56,9 +63,11 @@ namespace Covid19Project
         {
             try
             {
+                cnx.Open();
                 cmd.CommandText = "Delete From Suspect where cinsuspect = '" + cin + "';";
                 cmd.ExecuteNonQuery();
                 MessageBox.Show("Le suspect sera supprimé de la liste des suspects .");
+                cnx.Close();
             }
             catch (Exception ex)
             {
@@ -70,38 +79,63 @@ namespace Covid19Project
         {
             try
             {
+                cnx.Open();
                 cmd.CommandText = "Insert into patient(cinpatient,enquarantine) values('" + cin + "',false);" +//Ajouter au liste des patients
                 "Insert into historiquemaladie(cinPatient,dateMaladie) values('" + cin + "','" + dateTest + "');";
                 cmd.ExecuteNonQuery();
+                
+                cnx.Close();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+            
         }
+        public void deletePatient(string cin)
+        {
+            try
+            {
+                cnx.Open();
+                cmd.CommandText = "delete from patient where cinpatient='" + cin + "';";//supprimer au liste des patients
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
         //Vacciné
         public void insertVaccine(string cin, string nomVaccin, string dateVaccination)
         {
             try
             {
+                cnx.Open();
                 cmd.CommandText = "INSERT INTO vaccine(cinVaccine,nomVaccin,dateVaccination) " +
                         "Values('" + cin + "', '" + nomVaccin + "', '" + dateVaccination + "');" +//Insert into liste du vaccinés
-                        "Update carnetSanitaire set  faitvaccin = true,datevaccination ='" + dateVaccination + "' WHERE cincitoyen='" + cin + "';" +//Ajouter la vaccination au carnet sanitaire
+                        "Update carnetSanitaire set  faitvaccin = true,datevaccination ='" + dateVaccination + "' WHERE cincitoyen='" + cin + "';";//Ajouter la vaccination au carnet sanitaire
                 cmd.ExecuteNonQuery();
+                cnx.Close();
                 MessageBox.Show("Le citoyen a été ajouté à la liste des vacciné");
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+            
         }
         //Tests
         public void insertTest(string cin, string nomTest, string dateTest, string resultat)
         {
             try
             {
+                cnx.Open();
                 cmd.CommandText = "INSERT INTO test(cin,nomtest, datetest, resultat) VALUES('" + cin + "', '" + nomTest + "', '" + dateTest + "', " + resultat + ");";//Inserer le test
                 cmd.ExecuteNonQuery();
+                cnx.Close();
             }
             catch (Exception ex)
             {
@@ -113,10 +147,11 @@ namespace Covid19Project
         {
             try
             {
-                cmd.CommandText = "Delete From patient where cinpatient = '" + cin + "';" +
-                            "Insert into retablie(cinretablie,dateretablie) values('" + cin + "','" + dateRecuperation + "');";
+                cnx.Open();
+                cmd.CommandText = "Insert into retablie(cinretablie,dateretablie) values('" + cin + "','" + dateRecuperation + "');";
                 cmd.ExecuteNonQuery();
-                MessageBox.Show("Le patient n'est pas porteur du virus, il sera ajouté au liste des Rétablie.");
+                MessageBox.Show("Le patient sera ajouté au liste des Rétablies.");
+                cnx.Close();
             }
             catch (Exception ex)
             {
@@ -128,13 +163,77 @@ namespace Covid19Project
         { 
             try
             {
+                cnx.Open();
                 cmd.CommandText = "update citoyen set gravite = '" + etat + "' Where cincitoyen = '" + cin + "'";
                 cmd.ExecuteNonQuery();
+                cnx.Close();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
         }
+        //Mettre Patient en Quarantine
+        public void Mettre_Quarantine(string cin)
+        {
+            try
+            {
+                cnx.Open();
+                cmd.CommandText = "update patient set enquarantine = true Where cinpatient = '" + cin + "';";
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //
+        public void fillDataGrid(DataGridView d, string nameTable)
+        {
+            if(cnx.State == ConnectionState.Closed)
+                cnx.Open();
+            if (d.Name == "citoyenDataGrid")
+            {
+                string cmdString = "select * from " + nameTable;
+                cmd = new MySqlCommand(cmdString, cnx);
+                sda = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                d.DataSource = dt;
+                for (int i = 0; i < d.Rows.Count; i++)
+                {
+                    if (d.Rows[i].Cells[7].Value?.ToString() == "Faible")
+                    {
+                        d.Rows[i].Cells[7].Style.BackColor = Color.Green;
+                    }
+                    else if (d.Rows[i].Cells[7].Value?.ToString() == "Moyen")
+                    {
+                        d.Rows[i].Cells[7].Style.BackColor = Color.Orange;
+                    }
+                    else if (d.Rows[i].Cells[7].Value?.ToString() == "Haute")
+                    {
+                        d.Rows[i].Cells[7].Style.BackColor = Color.Red;
+                    }
+
+                }
+            }
+            else
+            {
+                string cmdString = "select * from " + nameTable + ",citoyen WHERE citoyen.cinCitoyen=" + nameTable + ".cin" + nameTable;
+                cmd = new MySqlCommand(cmdString, cnx);
+                sda = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dt.Columns.Remove("cinCitoyen");
+                d.DataSource = dt;
+            }
+            cnx.Close();
+        }
+        //Methode de Vaccination :
+
+
+        //Afficher le carnet sanitaire :
+
     }
 }
